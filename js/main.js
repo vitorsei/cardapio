4f82bf0a4a07343aed2993388a765ad226b67fc8
vitor <seiji.vitor@gmail.com>
2017-11-29 00:32:18 +1100

    V2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
-  $("#dish1").attr("src","./img/fried-rice.PNG");
-  $("#dish2").attr("src","./img/stir-fry.PNG");
+  $("#img1").attr("src","./img/fried-rice.PNG");
+  $("#img2").attr("src","./img/stir-fry.PNG");
+
+  $("#dishName1").text("PINEAPPLE & EGG FRIED RICE");
+  $("#dishName2").text("STIR FRY OF BUG TAIL & CHOPPED PRAWN");
+
+  $("#description1").text("spiced shredded chicken, cashew, mint & chilli");
+  $("#description2").text("egg noodle & hellfire chilli oil");
+
+  $("#price1").text("$25.5");
+  $("#price2").text("$28.5");
+
+  $("#allergy1").text("dairy & gluten free");
+  $("#allergy2").text("dairy free");
 });
